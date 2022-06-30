1c89d4e70f00ed0665145303b5af5df59da75750
daniel.ermeterio <ermeterio@gmail.com>
2022-06-29 23:11:46 -0300

    ci(Development course CI): :art: Insert ci.yml and tests

--- app/index.test.js
+++ app/index.test.js
@@ -1,15 +1,6 @@
 const index = require ('./index')
 
-function TestSum1()
-{
-   return index.Sum(3,4)
-}
-
-function TestSum2()
-{
-   return index.Sum(5,4)
-}
-
-console.log(TestSum1())
-
-console.log(TestSum2())
+test('Verificar Soma', () =>{
+    const result = index.Sum(3,4);
+    expect(result).toEqual(7);
+})
