4cdb3a661d50691cbb5f33ecac9df2c80673fec8
daniel.ermeterio <ermeterio@gmail.com>
2022-07-04 21:52:47 -0300

    ci(Development course CI):

--- sonar-project.js
+++ sonar-project.js
@@ -1,15 +1,16 @@
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
-        serverUrl:  'http://localhost:9000',
+        serverUrl:  'https://sonarcloud.io',
         options : {
-            'sonar.projectKey' : 'fc3-ci',
+            'sonar.projectKey' : 'ermeterio_fc3-ci',
+            'sonar.organization' : 'ermeterio',
             'sonar.sources':  '.',
             'sonar.tests':  '.',
             'sonar.inclusions'  :  '**', // Entry point of your code
             'sonar.test.inclusions':  'app/**/*.spec.js,app/**/*.spec.jsx,app/**/*.test.js,src/**/*.test.jsx',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
             'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
-            'sonar.login' : 'sqp_6c64f1a9160db76ce5ac36c80036663c669031df'
+            'sonar.login' : 'c1c03b70b0b1b8080981b2e78439bd9a54b88df9'
         }
     });
